80ed902b223eb8381a4bc7c8992153b70052b6e3
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-04-14 01:27:37 -0400

    Added images folder, with placeholder card images, and file paths in seed file.

--- scripts/cardSeed.js
+++ scripts/cardSeed.js
@@ -8,135 +8,142 @@ mongoose.connect(
 
 const cardSeed = [
     {
-        title: "Dumbledore Retreiver",
+        title: "Dumbledore Retriever",
         power: 9,
-        img: "",
+        img: "/assets/images/dumbledoreRetriever.PNG",
         flavor: "Expecto Pawtronum!"
     },
 
     {
         title: "Teenage Mutant Ninja Cat",
-        power: 6,
-        img: "",
+        power: 5,
+        img: "/assets/images/teenageMutantNinjaCat.PNG",
         flavor: ""
     },
 
     {
         title: "Omni-Cat",
         power: 10,
-        img: "",
+        img: "/assets/images/omniCat.PNG",
         flavor: "Galaxies. Lasers. Pizza."
     },
 
     {
         title: "Bombardier Kitty",
-        power: 6,
-        img: "",
+        power: 5,
+        img: "/assets/images/bombardierKitty.PNG",
         flavor: "EXPLOSIONS!!!"
     },
 
     {
         title: "Bob",
         power: 2,
-        img: "",
+        img: "/assets/images/bob.PNG",
         flavor: "You have cat to be kitten me, right meow."
     },
 
     {
         title: "Thug Pug",
         power: 2,
-        img: "",
+        img: "/assets/images/thugPug.PNG",
         flavor: ""
     },
 
     {
         title: "Santa Paws",
         power: 4,
-        img: "",
+        img: "/assets/images/santaPaws.PNG",
         flavor: ""
     },
 
     {
         title: "Pug In A Rug",
         power: 4,
-        img: "",
+        img: "/assets/images/pugInARug.PNG",
         flavor: ""
     },
 
     {
         title: "Jacked Rabbit",
-        power: 8,
-        img: "",
+        power: 6,
+        img: "/assets/images/jackedRabbit.PNG",
         flavor: "I like to lift things and then put them back down."
     },
 
     {
         title: "Crossfit Cat",
-        power: 5,
-        img: "",
+        power: 4,
+        img: "/assets/images/crossfitCat.PNG",
         flavor: "Do you even cat, bro?"
     },
 
     {
         title: "Fluffy",
         power: 7,
-        img: "",
+        img: "/assets/images/fluffy.PNG",
         flavor: "So cute, ya just want to pet 'em."
     },
 
     {
         title: "Skittles, the Destroyer",
         power: 10,
-        img: "",
+        img: "/assets/images/skittlesTheDestroyer.PNG",
         flavor: "Skittles invokes destruction to all who gaze upon its cuteness."
     },
 
     {
         title: "Bravery Incarnate",
         power: 3,
-        img: "",
+        img: "/assets/images/braveryIncarnate.PNG",
         flavor: "Sure.  I suppose bravery comes in all shapes and sizes."
     },
 
     {
-        title: "",
-        power: "",
-        img: "",
+        title: "Jabba the Nutt",
+        power: 3,
+        img: "/assets/images/jabbaTheNutt.PNG",
+        flavor: "I will not give up my favorite nut."
+    },
+
+    {
+        title: "Cat in the Hat",
+        power: 4,
+        img: "/assets/images/catInTheHat.PNG",
+        flavor: "I mustache you a question."
+    },
+
+    {
+        title: "The Grey Wizard",
+        power: 7,
+        img: "/assets/images/theGreyWizard.PNG",
+        flavor: "You shall not pass!"
+    },
+
+    {
+        title: "Tuxedo Chicken",
+        power: 4,
+        img: "/assets/images/tuxedoChicken.PNG",
+        flavor: "You are a chicken, right?"
+    },
+
+    {
+        title: "Disco Chick",
+        power: 3,
+        img: "/assets/images/discoChick.PNG",
         flavor: ""
     },
 
     {
-        title: "",
-        power: "",
-        img: "",
-        flavor: ""
+        title: "Snail Mail",
+        power: 2,
+        img: "/assets/images/snailMail.PNG",
+        flavor: "Perhaps you should have overnighted that package?"
     },
 
     {
-        title: "",
-        power: "",
-        img: "",
-        flavor: ""
-    },
-
-    {
-        title: "",
-        power: "",
-        img: "",
-        flavor: ""
-    },
-
-    {
-        title: "",
-        power: "",
-        img: "",
-        flavor: ""
-    },
-
-    {
-        title: "",
-        power: "",
-        img: "",
+        title: "Count Flufferton",
+        power: 6,
+        img: "/assets/images/countFlufferton",
         flavor: ""
     }
 ]
